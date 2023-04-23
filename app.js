2df45171d517a40af5382623329a97971ba0279c
mohamed El-Raghy <elraghy8@gmail.com>
2023-04-23 03:07:13 +0200

    first commit & setting up Express server

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+
+app.get('/', (req, res, next) => {
+  res.send('Hello World!');
+})
+
+
+app.listen(3000, ()=> {
+  console.log(`app listening on port`);
+})

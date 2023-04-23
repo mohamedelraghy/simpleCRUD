262c76c1cd6576738881bda9ee59f3bb8520c3c4
mohamed El-Raghy <elraghy8@gmail.com>
2023-04-24 00:31:53 +0200

    solving cors problem

--- app.js
+++ app.js
@@ -6,6 +6,19 @@ const app = express();
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 app.use('/post', postRoute);
 
 app.use((error, req, res, next) => {
